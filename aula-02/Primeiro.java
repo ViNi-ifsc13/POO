bcc399239850937f60e6132633b181a3b3cbe17e
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-02-19 20:18:19 -0300

    Revisão sobre java

---
+++ aula-02/Primeiro.java
@@ -0,0 +1,26 @@
+void main(){
+    IO.println("Olá, mundo!");
+    
+    int i = 1;
+    String s ;
+
+    switch (i) {
+        case 1:
+            s = "Um";
+            System.out.println(s);         
+            break;
+        case 2:
+            s = "Dois";  
+            System.out.println(s);           
+            break;    
+        default:
+            s = "Outro";
+            System.out.println(s);   
+            break;
+    }
+
+    
+
+
+    
+}

77d03f3aa462ba4c8d5917c041b4b4124e3846e2
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-02-26 20:23:26 -0300

    Começo exercício 1 lab-02

---
+++ lab-02/exercicio-01/src/main/java/ads/poo/Main.java
@@ -0,0 +1,29 @@
+package ads.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TODO terminar isso ai
+        int dia = Integer.parseInt( IO.readln("Entre com o dia:"));
+        String mes = (IO.readln("Entre com o mês:"));
+
+
+        String estaçao;
+
+        if (dia > 20 && mes.equals("março")  || mes.equals("abril") || mes.equals("maio") || dia < 21 && mes.equals("junho"))
+            estaçao = Outono;
+        else if(dia > 20 && mes.equals("junho") || mes.equals("julho") || mes.equals("agosto") || dia < 23 && mes.equals("setembro"))
+            estaçao = Inverno;
+        else if (dia > 22 && mes.equals("setembro") || mes.equals("outubro") || mes.equals("novemnro") || dia < 21 && mes.equals("dezembro"))
+            estaçao = Primavera;
+        else if (dia > 20 && mes.equals("dezembro") || mes.equals("janeiro") || mes.equals("fevereiro") || dia < 21 && mes.equals("março"))
+            estaçao = Verão;
+        }
+
+        IO.println("A estação é:" + estaçao);
+
+
+        }
+    }
+}

82c4421d0a4b198568b5efce82ec8c0bffdfc69e
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-03 20:21:09 -0300

    Começando exercício 3 lab2

--- lab-02/exercicio-03/src/main/java/ads/poo/Main.java
+++ lab-02/exercicio-03/src/main/java/ads/poo/Main.java
@@ -3,10 +3,21 @@ package ads.poo;
 public class Main {
     static void main(String[] args) {
 
+        //TODO terminar essa atividade
+
         if (args.length == 0) {
             IO.println("Não forneceu argumentos de linha de comando");
-        }else{
-            IO.println("Primeiro argumento: " + args[0]);
+        }else if (args[0].equals("triângulo"));
+
+        int tamanho = args[1];
+        for (int i = 0; i < tamanho; i++) {
+            IO.println("*");
+
+        }
+
+
+
+
         }
     }
 }

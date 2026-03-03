7b6741ddd08845c452aff9657de29425b40aa92e
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-03 20:04:34 -0300

    Concluindo exercício 2

--- lab-02/exercicio-02/src/main/java/ads/poo/Main.java
+++ lab-02/exercicio-02/src/main/java/ads/poo/Main.java
@@ -6,20 +6,26 @@ public class Main {
     static void main() {
         Random r = new Random();
         int i = r.nextInt(101);
+        int tentativas = 0;
 
-        int palpite = 0;
+
         IO.println("Acerte um número de 1 a 100!");
-        IO.readln("Entre com um número: ");
+        int palpite = 0;
 
 
-
-
-
-
+       while (palpite != i) {
+           palpite = Integer.parseInt(IO.readln("Entre com um número: "));
+           if (palpite > i)
+               IO.println("Seu palpite foi maior que o número!");
+           else {
+               IO.println("Seu palpite foi menor que o número!");
+           }
+           tentativas++;
+       }
 
 
         IO.println("Parabéns você acertou!!");
-        IO.println("Foram necessáris " + tentativas + "tentativas!");
+        IO.println("Foram necessárias " + tentativas + " tentativas!");
     }
 
 }
---
+++ lab-02/exercicio-03/src/main/java/ads/poo/Main.java
@@ -0,0 +1,12 @@
+package ads.poo;
+
+public class Main {
+    static void main(String[] args) {
+
+        if (args.length == 0) {
+            IO.println("Não forneceu argumentos de linha de comando");
+        }else{
+            IO.println("Primeiro argumento: " + args[0]);
+        }
+    }
+}

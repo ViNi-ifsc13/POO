6cd4660a3aab8fcd37f7f0386d3c00a514317478
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-03 19:14:24 -0300

    Concertando exercício 1

--- lab-02/exercicio-01/src/main/java/ads/poo/Main.java
+++ lab-02/exercicio-01/src/main/java/ads/poo/Main.java
@@ -5,27 +5,26 @@ package ads.poo;
 public class Main {
     static void main() {
         //TODO terminar isso ai
-        int dia = Integer.parseInt( IO.readln("Entre com o dia:"));
+        int dia = Integer.parseInt(IO.readln("Entre com o dia:"));
         String mes = (IO.readln("Entre com o mês:"));
 
 
-        String estaçao;
+        String estacao = "";
 
-        if (dia > 20 && mes.equals("março")  || mes.equals("abril") || mes.equals("maio") || dia < 21 && mes.equals("junho"))
-            estaçao = Outono;
-        else if(dia > 20 && mes.equals("junho") || mes.equals("julho") || mes.equals("agosto") || dia < 23 && mes.equals("setembro"))
-            estaçao = Inverno;
+        if (dia > 20 && mes.equals("março") || mes.equals("abril") || mes.equals("maio") || dia < 21 && mes.equals("junho"))
+            estacao = "Outono";
+        else if (dia > 20 && mes.equals("junho") || mes.equals("julho") || mes.equals("agosto") || dia < 23 && mes.equals("setembro"))
+            estacao = "Inverno";
         else if (dia > 22 && mes.equals("setembro") || mes.equals("outubro") || mes.equals("novemnro") || dia < 21 && mes.equals("dezembro"))
-            estaçao = Primavera;
+            estacao = "Primavera";
         else if (dia > 20 && mes.equals("dezembro") || mes.equals("janeiro") || mes.equals("fevereiro") || dia < 21 && mes.equals("março"))
-            estaçao = Verão;
-        else
+            estacao = "Verão";
+        else {
             System.out.println("Erro");
         }
 
-        IO.println("A estação é:" + estaçao);
+        IO.println("A estação é: " + estacao);
 
 
-        }
     }
 }

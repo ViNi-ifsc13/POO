46f22911316be72d5d41c9e1cf918e2ccd908a37
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-03 19:28:15 -0300

    Começando exercício 2 lab2

--- lab-02/exercicio-02/src/main/java/ads/poo/Main.java
+++ lab-02/exercicio-02/src/main/java/ads/poo/Main.java
@@ -1,7 +1,25 @@
 package ads.poo;
 
+import java.util.Random;
+
 public class Main {
+    static void main() {
+        Random r = new Random();
+        int i = r.nextInt(101);
+
+        int palpite = 0;
+        IO.println("Acerte um número de 1 a 100!");
+        IO.readln("Entre com um número: ");
 
 
+
+
+
+
+
+
+        IO.println("Parabéns você acertou!!");
+        IO.println("Foram necessáris " + tentativas + "tentativas!");
+    }
 
 }

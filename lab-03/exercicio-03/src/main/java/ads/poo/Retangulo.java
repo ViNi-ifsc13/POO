cd877e2c48530acbee50f83dd68c6addba911483
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-26 20:20:34 -0300

    Começo exercicio 3 lab-03

---
+++ lab-03/exercicio-03/src/main/java/ads/poo/Retangulo.java
@@ -0,0 +1,28 @@
+package ads.poo;
+
+public class Retangulo {
+
+    // Atributos
+        private int largura;
+        private int altura;
+        private String cod;
+
+
+    // Métodos construtor
+
+    public Retangulo() {
+        this.largura = 4;
+        this.altura = 3;
+        this.cod = "ASCII";
+    }
+
+    public Retangulo(int largura, int altura, String cod) {
+        this.largura = largura;
+        this.altura = altura;
+        this.cod = cod;
+    }
+
+    // Métodos
+
+
+}

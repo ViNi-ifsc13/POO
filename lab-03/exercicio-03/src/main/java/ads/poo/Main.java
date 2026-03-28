890e0f6bdc927a03f200f625c24f8f29c0401dba
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-03-28 14:31:42 -0300

    Atualizações na atividade 3 do lab-03

--- lab-03/exercicio-03/src/main/java/ads/poo/Main.java
+++ lab-03/exercicio-03/src/main/java/ads/poo/Main.java
@@ -3,5 +3,15 @@ package ads.poo;
 public class Main {
     static void main() {
 
+        Retangulo r = new Retangulo();
+
+        IO.println(r.setAltura(10));
+        IO.println(r.setLargura(10));
+        IO.println(r.setCod("ASCII"));
+
+        IO.println(r.area());
+        IO.println(r.perimetro());
+        IO.println(r.toString());
+
         }
     }
--- lab-03/exercicio-03/src/main/java/ads/poo/Retangulo.java
+++ lab-03/exercicio-03/src/main/java/ads/poo/Retangulo.java
@@ -10,6 +10,16 @@ public class Retangulo {
 
     // Métodos construtor
 
+
+    @Override
+    public String toString() {
+        return
+                "*" + "-".repeat(largura - 2) + "*" +
+                ("\n|" + " ".repeat(largura - 2) + "|").repeat(altura - 2) +
+                "\n*" + "-".repeat(largura - 2) + "*"
+                ;
+    }
+
     public Retangulo() {
         this.largura = 4;
         this.altura = 3;
@@ -24,5 +34,55 @@ public class Retangulo {
 
     // Métodos
 
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public boolean setLargura(int largura) {
+        if (largura < 4){
+            return false;
+        }else{
+            this.largura = largura;
+            return true;
+        }
+
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public boolean setAltura(int altura) {
+        if (altura < 5){
+            return false;
+        }else {
+            this.altura = altura;
+            return true;
+        }
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public boolean setCod(String cod) {
+        if (cod == "ASCII" || cod == "UTF8"){
+            this.cod = cod;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public double area(){
+        int area = this.altura * this.largura;
+        return area;
+    }
+
+    public double perimetro(){
+        int peri = 2 * (this.altura + this.largura);
+        return peri;
+    }
 
 }

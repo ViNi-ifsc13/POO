3d8a9a71f5be364cb1015c752c048529e2b962ec
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-05-18 12:52:55 -0300

    Conclusão do sistema de livros, aula-17 HashMap

--- aula-17/src/main/java/ads/poo/App.java
+++ aula-17/src/main/java/ads/poo/App.java
@@ -1,6 +1,7 @@
 package ads.poo;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class App {
 
@@ -22,61 +23,78 @@ public class App {
     }
 
     void listarLivros() {
-        for (Livro elemento : livros) {
-            IO.println(elemento.toString());
-        }
+        livros.forEach((isbn, livro) ->
+                IO.println(isbn + " - " + livro));
     }
 
     void listarLivrosAno() {
+        var ano = Integer.parseInt(IO.readln("Entre com o ano do livro: "));
+
+
+        for (Livro livro : livros.values()) {
+
+            var anoDoLivro = livro.getAno();
+            if (anoDoLivro == ano) {
+                IO.println(livro.toString());
+            }
+        }
     }
 
     void atualizarLivros() {
-    }
+        var isbn = IO.readln("Insira o ISBN do livro que deseja modificar: ");
 
+
+        for (Livro livro : livros.values()) {
+            var livroSelecionado = livro.getIsbn();
+            if (Objects.equals(isbn, livroSelecionado)) {
+                IO.println("Agora insira as informações atualizadas.");
+
+
+                var nome = IO.readln("Entre com o nome do livro: ");
+                var genero = IO.readln("Entre com o gênero do livro: ");
+                var autor = IO.readln("Entre com o nome do autor do livro: ");
+                var ano = Integer.parseInt(IO.readln("Entre com o ano de lançamento do livro: "));
+
+                livro.setNome(nome);
+                livro.setGenero(genero);
+                livro.setAutor(autor);
+                livro.setAno(ano);
+            }
+        }
+    }
 
     void menu(int opcao) {
         while (opcao != 5) {
+
+            String hub = String.format ("""
+                    ..:: Menu ::..
+                    1 - Cadastrar livro
+                    2 - Listar todos os livros
+                    3 - Listar todos os livros publicados em um ano específico
+                    4 - Alterar dados de um livro
+                    5 - Sair""");
+            IO.println(hub);
+
             opcao = Integer.parseInt(IO.readln("Entre com uma opção: "));
 
             switch (opcao) {
 
-                case 1 -> {
-                    cadastrar();
-                }
+                case 1 -> {cadastrar();}
 
-                case 2 -> {
-                    listarLivros();
-                }
+                case 2 -> {listarLivros();}
 
-                case 3 -> {
-                    listarLivrosAno();
-                }
+                case 3 -> {listarLivrosAno();}
 
-                case 4 -> {
-                    atualizarLivros();
-                }
+                case 4 -> {atualizarLivros();}
 
-                case 5 -> {
+                case 5 -> {IO.println("Encerrando sistema...");}
                 }
-
             }
-
         }
-    }
-
 
     static void main(String[] args) {
-
-        // Sistema para gestão de acervo de uma Livraria
-        // 1 - Cadastrar livro
-        // 2 - Listar todos os livros
-        // 3 - Listar todos os livros publicados em um ano específico
-        // 4 - Alterar dados de um livro
-        // 5 - Sair
-
         App app = new App();
-        app.cadastrar();
-
+        app.menu(0);
 
     }
 }
